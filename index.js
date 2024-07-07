e03f01e760f50dbb99388d07554c3dde59df4d3f
Abass Ibrahim <profbass4all@gmail.com>
2024-07-07 13:38:46 +0100

    implementing mysql

--- index.js
+++ index.js
@@ -3,80 +3,99 @@ const express = require('express')
 const joi = require('joi')
 const app = express()
 const port = process.env.PORT 
+const mysql = require('mysql')
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json())
 
+
+const mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root123',
+    database: 'cms',
+    port: 3306 
+})
+
+mysqlConnection.connect((error)=>{
+    if(error){
+        console.log('not connected')
+    }else{
+        console.log('connected')
+    }
+})
 app.listen(port, ()=>{
     console.log(`listening to port ${port}`)
-}) 
+    
+        })
 
 //user management
-const users =[
-    {
-        firstName: 'Sherifat',
-        lastName: 'Oshoala',
-        userId: 1,
-        dob: '12/06/1990',
-        stateOfOrigin: 'lagos',
-        role: 'user',
-        phoneNo: '08102023380',
-        password: 'sheriprinkle',
-        confirmPassword: 'sheriprinkle',
-        maritalStatus: 'contrived',
-        qualification: 'masters',
-        articles: [],
-        noOfArticles: 0,
-        avatar: 'image',
-    },
-    {
-        firstName: 'Barakat',
-        lastName: 'Asunke',
-        userId: 2,
-        dob: '12/06/1990',
-        stateOfOrigin: 'osun',
-        role: 'user',
-        phoneNo: '08112023380',
-        password: 'sheriprinkle',
-        confirmPassword: 'sheriprinkle',
-        maritalStatus: 'married',
-        qualification: 'ssce',
-        articles: [],
-        noOfArticles: 0,
-        avatar: 'image',
-    },
-    {
-        firstName: 'Tayo',
-        lastName: 'Bakare',
-        userId: 3,
-        dob: '12/06/1990',
-        stateOfOrigin: 'Kwara',
-        role: 'user',
-        phoneNo: '08102024480',
-        password: 'sheriprinkle',
-        confirmPassword: 'sheriprinkle',
-        maritalStatus: 'single',
-        qualification: 'undergraduate',
-        articles: [],
-        noOfArticles: 0,
-        avatar: 'image',
-    },
-    {
-        firstName: 'Bayo',
-        lastName: 'Bakare',
-        userId: 4,
-        dob: '12/06/1990',
-        stateOfOrigin: 'Kwara',
-        role: 'user',
-        phoneNo: '08102024480',
-        password: 'sheriprinkle',
-        confirmPassword: 'sheriprinkle',
-        maritalStatus: 'single',
-        qualification: 'undergraduate',
-        articles: [],
-        noOfArticles: 0,
-        avatar: 'image',
-    },
-]
+// const users =[
+//     {
+//         firstName: 'Sherifat',
+//         lastName: 'Oshoala',
+//         userId: 1,
+//         dob: '12/06/1990',
+//         stateOfOrigin: 'lagos',
+//         role: 'user',
+//         phoneNo: '08102023380',
+//         password: 'sheriprinkle',
+//         confirmPassword: 'sheriprinkle',
+//         maritalStatus: 'contrived',
+//         qualification: 'masters',
+//         articles: [],
+//         noOfArticles: 0,
+//         avatar: 'image',
+//     },
+//     {
+//         firstName: 'Barakat',
+//         lastName: 'Asunke',
+//         userId: 2,
+//         dob: '12/06/1990',
+//         stateOfOrigin: 'osun',
+//         role: 'user',
+//         phoneNo: '08112023380',
+//         password: 'sheriprinkle',
+//         confirmPassword: 'sheriprinkle',
+//         maritalStatus: 'married',
+//         qualification: 'ssce',
+//         articles: [],
+//         noOfArticles: 0,
+//         avatar: 'image',
+//     },
+//     {
+//         firstName: 'Tayo',
+//         lastName: 'Bakare',
+//         userId: 3,
+//         dob: '12/06/1990',
+//         stateOfOrigin: 'Kwara',
+//         role: 'user',
+//         phoneNo: '08102024480',
+//         password: 'sheriprinkle',
+//         confirmPassword: 'sheriprinkle',
+//         maritalStatus: 'single',
+//         qualification: 'undergraduate',
+//         articles: [],
+//         noOfArticles: 0,
+//         avatar: 'image',
+//     },
+//     {
+//         firstName: 'Bayo',
+//         lastName: 'Bakare',
+//         userId: 4,
+//         dob: '12/06/1990',
+//         stateOfOrigin: 'Kwara',
+//         role: 'user',
+//         phoneNo: '08102024480',
+//         password: 'sheriprinkle',
+//         confirmPassword: 'sheriprinkle',
+//         maritalStatus: 'single',
+//         qualification: 'undergraduate',
+//         articles: [],
+//         noOfArticles: 0,
+//         avatar: 'image',
+//     },
+// ]
 //endpoint to create user using joi to validate the input
 app.post('/api/user', (request, response)=>{
     try {
@@ -94,38 +113,34 @@ app.post('/api/user', (request, response)=>{
             "any.required": `"firsName" is required bro`
         }),
         dob: joi.string().trim().alphanum().required(),
-        password: joi.string().trim().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-        confirmPassword: joi.ref('password')
+        currentRole:joi.string().trim().required(),
+        currentPassword: joi.string().trim().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+        confirmPassword: joi.ref('currentPassword')
     })
-    .with('password', 'confirmPassword')
+    // .with('password', 'confirmPassword')
 
     const {error, value} = createUserSchema.validate(request.body)
-    const {firstName, lastName, dob, password, confirmPassword} = value
-    
+    const{firstName, lastName, dob, currentPassword, confirmPassword, currentRole} = value
     if(error){
         throw new Error (error.details[0].message)
     }
-
-    users.push({
-        firstName,
-        lastName,
-        userId: users.length +1,
-        dob,
-        stateOfOrigin: null,
-        role: 'user',
-        phoneNo: null,
-        password,
-        confirmPassword,
-        maritalStatus: null,
-        qualification: null,
-        articles: [],
-        noOfArticles: 0,
-        avatar: null,
-    })  
+    mysqlConnection.query(
+        {
+            sql : `INSERT INTO cms (userId, firstName, lastName, dob, currentPassword, confirmPassword, currentRole) VALUES (?,?,?,?,?, ?,?)`,
+            values :[uuidv4(), firstName, lastName, dob, currentPassword, confirmPassword, currentRole]
+        },
+        (err, result, field) =>{
+            if(err){
+                console.log(err.message)
+            }else{
+                console.log(`stuffs added`)
+            }
+        }
+    )
     response.status(201).json({
         message: 'Success',
         status: true,
-        data: users
+        data: value
     })
 
     } catch (error) {
@@ -135,22 +150,84 @@ app.post('/api/user', (request, response)=>{
         })
     }
 })
+//endpoint to get all users
+app.get('/api/users', (request, response) =>{
+    try {
+        const {firstName, currentRole} = request.query
+        console.log(request.query)
+
+    if(!firstName && !currentRole){
+        mysqlConnection.query({
+            sql: `SELECT * FROM cms`
+        },
+        (err, result, fields)=>{
+            if(err){
+                console.log(err)
+            }else{
+                response.status(200).json({
+                message: 'success',
+                status: true,
+                data: result
+                })
+            }
+        }
+    )
+    }else{
+        let key = ''
+        let val = []
+        for(let data in request.query){
+            key+= `${data}=? and `
+            val.push(request.query[data])
+        }
+        key = key.slice(0,-4)
+        console.log(key)
+        mysqlConnection.query({
+        sql: `SELECT * FROM cms WHERE ${key}`,
+        values: [...val]
+        },
+        (err, result, fields)=>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log(result)
+                response.status(200).json({
+                message: 'success',
+                status: true,
+                data: result
+                })
+            }
+        })
+    }   
+    
+    } catch (error) {
+        response.status(400).json({
+            message: error.message,
+            status: false
+        })
+    }
+    
+})
+
 
 //endpoint to get a single user
 app.get('/api/user/:userId', (request, response)=>{
     const {userId} = request.params
     try{
-        const user = users.find(user => user.userId == userId)
-
-        if(!user){
-            throw new Error (`This user is not available`)
-        }
+        mysqlConnection.query({
+          sql: `SELECT * FROM cms WHERE userId = ?`,
+          values: [userId]  
+        },
+        (err, result, field)=>{
+            if(err) console.log(err)
+            console.log(result)
         response.status(200).json({
             message:'Successfull',
             status: true,
-            data: user
+            data: result
         })
-    }catch(error){
+        }
+        )
+        }catch(error){
         response.status(400).json({
             message: error.message,
             status: false
@@ -163,19 +240,19 @@ app.patch('/api/user/:userId', (request, response)=>{
     const {userId} = request.params
     try {
         const updateUserSchema = joi.object({
-            firstName: joi.string().trim().pattern(/^[a-zA-Z]{3,40}$/).min(3).max(40).required().messages({
+            firstName: joi.string().trim().pattern(/^[a-zA-Z]{3,40}$/).min(3).max(40).messages({
             "string.min": `"firstName" must be greater {#limit}`,
             "string.max": `"firstName" must be less than {#limit}`,
             "string.empty": `"firstName" cannot be empty`,
             "any.required": `"firsName" is required bro`
         }),
-        lastName: joi.string().trim().pattern(/^[a-zA-Z]{3,40}$/).min(3).max(40).required().messages({
+        lastName: joi.string().trim().pattern(/^[a-zA-Z]{3,40}$/).min(3).max(40).messages({
             "string.min": `"firstName" must be greater {#limit}`,
             "string.max": `"firstName" must be less than {#limit}`,
             "string.empty": `"firstName" cannot be empty`,
             "any.required": `"firsName" is required bro`
         }),
-        dob: joi.string().trim().alphanum().required(),
+        dob: joi.string().trim().alphanum(),
         stateOfOrigin: joi.string().trim().pattern(new RegExp('^[a-zA-Z]$')),
         phoneNo: joi.string().trim().pattern(/^\+\(\d{3}\)-?\d{3} \d{3} \d{4}$/),
         password: joi.string().trim().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
@@ -186,17 +263,36 @@ app.patch('/api/user/:userId', (request, response)=>{
     })
     const {error, value} = updateUserSchema.validate(request.body)
     if(error) throw new Error (error.details[0].message)
-    for(let i = 0; i < users.length; i++){
-        if(users[i].userId == userId){
-            users[i] = {...users[i], ...value}
-        }
+    // for(let i = 0; i < users.length; i++){
+    //     if(users[i].userId == userId){
+    //         users[i] = {...users[i], ...value}
+    //     }
+    // }
+    let key = ''
+    let values = []
+    for(let data in value){
+        key += `${data}=?,`
+        values.push(value[data])
+        console.log(values)
     }
-
-    response.status(200).json({
+    
+    let updatedKey = key.slice(0, -1)
+    console.log(updatedKey)
+    mysqlConnection.query({
+        sql: `UPDATE cms SET ${updatedKey} WHERE userId = ?`,
+        values: [...values, userId]
+    },  
+       (err, results, fields)=>{
+        if(err) throw new Error(err)
+            console.log(results)
+        response.status(200).json({
         message: 'successful',
         status: true,
-        data: users
+        data: results
     })
+       } 
+)
+    
     } catch (error) {
         response.status(400).json({
             message: error.message,
@@ -264,56 +360,69 @@ app.post('/api/article/:userId', (request, response)=>{
         const {userId} = request.params
         const createArticleSchema = joi.object({
             title: joi.string().trim().required(),
-            author: joi.string().trim().pattern(/^[a-zA-Z]+$/).min(3).max(40).required(),
             category: joi.string().trim().required(),
             content: joi.string().trim().max(1000)
         })
         const {error, value} = createArticleSchema.validate(request.body)
         if( error) throw new Error (error.details[0].message)
-        const {title, author, category, content} = value
+        const {title, category, content} = value
 
-        for(let i = 0; i < articles.length; i++){
-            if(articles[i].title == title && articles[i].author == author){
-                throw new Error(`You've posted an article with this same title`)
-            }
-        }
+        // for(let i = 0; i < articles.length; i++){
+        //     if(articles[i].title == title && articles[i].author == author){
+        //         throw new Error(`You've posted an article with this same title`)
+        //     }
+        // }
         
-        const getCategory = categories.find(obj => obj.category == category)
-        if(getCategory){
-            getCategory.count+= 1
-        }else{
-            categories.push({
-                category,
-                count: 1
-            })
-        }
-        for(let i = 0; i< users.length; i++){
-            if(users[i].userId == userId){
-                users[i].articles.push({
-                    articleId: articles.length +1,
-                    title,
-                    author,
-                    category,
-                    date: new Date().toDateString(),
-                    content
+        // const getCategory = categories.find(obj => obj.category == category)
+        // if(getCategory){
+        //     getCategory.count+= 1
+        // }else{
+        //     categories.push({
+        //         category,
+        //         count: 1
+        //     })
+        // }
+        // for(let i = 0; i< users.length; i++){
+        //     if(users[i].userId == userId){
+        //         users[i].articles.push({
+        //             articleId: articles.length +1,
+        //             title,
+        //             author,
+        //             category,
+        //             date: new Date().toDateString(),
+        //             content
+        //         })
+        //         users[i].noOfArticles = users[i].articles.length
+        //     }
+        // }
+        // articles.push({
+        //     userId,
+        //     articleId: articles.length +1,
+        //     title,
+        //     author,
+        //     category,
+        //     date: new Date().toDateString(),
+        //     content
+        // })
+        mysqlConnection.query({
+            sql: `INSERT INTO articles (articleId, userId, title, category, dateOfPosting, content) Values (?,?,?,?,Now(),?)`,
+            values: [uuidv4(), userId, title, category, content ]
+        },
+        (err, result, field)=>{
+            if(err){
+                console.log(err)
+            }else{
+                console.log('stuffss added')
+                response.status(200).json({
+                message: 'success',
+                status: true,
+                data: value
                 })
-                users[i].noOfArticles = users[i].articles.length
             }
         }
-        articles.push({
-            userId,
-            articleId: articles.length +1,
-            title,
-            author,
-            category,
-            date: new Date().toDateString(),
-            content
-        })
-        response.status(200).json({
-            message: 'success',
-            status: true,
-            data: articles
-        })
+    
+    )
+        
     } catch (error) {
         response.status(400).json({
             message: error.message,
@@ -327,6 +436,7 @@ app.post('/api/article/:userId', (request, response)=>{
 app.get('/api/articles', (request, response) =>{
     try {
         const {category} = request.query
+        console.log(request.query)
         let post = articles
         if(category){
             post = articles.filter(post => post.category == category)
@@ -334,11 +444,23 @@ app.get('/api/articles', (request, response) =>{
 
         if(!post) throw new Error(`No article available`)
         if(post.length == 0) throw new Error ('No article available')
-        response.status(200).json({
+mysqlConnection.query({
+    sql: `SELECT * FROM articles`
+    },
+    (err, result, fields)=>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log(result)
+            response.status(200).json({
         message: 'success',
         status: true,
-        data: post
+        data: result
     })
+        }
+    }
+)
+        
     } catch (error) {
         response.status(400).json({
             message: error.message,
