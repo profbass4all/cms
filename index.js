2d4bf8b9881a7dc1290dc9384c49677f53cc6257
Abass Ibrahim <profbass4all@gmail.com>
2024-07-07 13:43:22 +0100

    utilised .env for connection

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ app.use(express.json())
 
 
 const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root123',
-    database: 'cms',
-    port: 3306 
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    port: process.env.DATABASE_PORT
 })
 
 mysqlConnection.connect((error)=>{
